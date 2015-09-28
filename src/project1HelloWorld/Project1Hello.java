b96c9c36195f8e4dc50c9bc39ad6cec5e55f6e19
Armin <Armin@Armin.mobile.asu.edu>
2015-09-27 19:00:01 -0700

    temp try 4 now

---
+++ src/project1HelloWorld/Project1Hello.java
@@ -0,0 +1,10 @@
+package project1HelloWorld;
+
+public class Project1Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello world");
+	}
+
+}
