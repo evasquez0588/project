779f8e6c11872b5e3971c7f73655865e648979d2
jshaller <jshaller@asu.edu>
2015-10-09 17:17:59 -0700

    Create PAT_HCU.java

---
+++ src/GUI/PAT_HCU.java
@@ -0,0 +1,23 @@
+package phase1;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class PAT_HCU extends JPanel {
+
+	/**
+	 * This is a stand in panel for the patient health care update JPanel
+	 */
+	public PAT_HCU() {
+		setLayout(null);
+		setSize(343,210);
+		
+		JLabel lblScheduleAppt = new JLabel("Health Condition Update");
+		lblScheduleAppt.setBounds(95, 11, 128, 14);
+		add(lblScheduleAppt);
+		
+		JLabel lblInsertDesignHere = new JLabel("INSERT DESIGN HERE");
+		lblInsertDesignHere.setBounds(10, 63, 135, 14);
+		add(lblInsertDesignHere);
+	}
+}
