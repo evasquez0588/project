04fac3b7dcef58e3db36f3c69bcf860840810b64
jshaller <jshaller@asu.edu>
2015-10-09 17:03:54 -0700

    Create ApplicationRunner.java

---
+++ src/GUI/ApplicationRunner.java
@@ -0,0 +1,8 @@
+public class ApplicationRunner {
+	public static void main(String[] args){
+		//TestPatient_Registration1 a = new TestPatient_Registration1();
+		//a.display();
+		Testv2 goobypls = new Testv2();
+		goobypls.display();
+	}
+}
