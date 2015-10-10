14e7ad2705b9899710556090bdc9a28bc1f45ec4
jshaller <jshaller@asu.edu>
2015-10-09 17:07:23 -0700

    Update ApplicationRunner.java

--- src/GUI/ApplicationRunner.java
+++ src/GUI/ApplicationRunner.java
@@ -2,7 +2,7 @@ public class ApplicationRunner {
 	public static void main(String[] args){
 		//TestPatient_Registration1 a = new TestPatient_Registration1();
 		//a.display();
-		Testv2 goobypls = new Testv2();
+		ApplicationGUI goobypls = new ApplicationGUI();
 		goobypls.display();
 	}
 }
