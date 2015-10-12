063415a6b637c82c583c321f1ae6af1be42799f0
jshaller <jshaller@asu.edu>
2015-10-11 22:22:15 -0700

    Update ApplicationGUI.java
    reg_LI params

--- src/GUI/ApplicationGUI.java
+++ src/GUI/ApplicationGUI.java
@@ -82,7 +82,7 @@ public class ApplicationGUI extends JFrame {
 		RegistrationBase = new TestPatient_Registration1(e, b);
 		reg_PI = new Reg_PI(b);
 		reg_II = new Reg_II(b);
-		reg_LI = new Reg_LI();
+		reg_LI = new Reg_LI(b);
 		reg_CI = new Reg_CI();
 		reg_MH = new Reg_MH(b);
 
