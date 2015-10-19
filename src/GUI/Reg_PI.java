0670107d7d2100a04897d2c3990601d6cec1e665
JasonCai <JasonCai@JasonCai-PC.mobile.asu.edu>
2015-10-18 19:11:49 -0700

    fix radio button for gender

--- src/GUI/Reg_PI.java
+++ src/GUI/Reg_PI.java
@@ -4,6 +4,7 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JCheckBox;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -16,7 +17,8 @@ public class Reg_PI extends JPanel {
 	public JTextField firstNameTextField;
 	public JRadioButton maleCheck, femaleCheck;
 	public JButton save;
-
+	public ButtonGroup sex;
+	
 	/**
 	 * Create the panel.
 	 */
@@ -32,38 +34,42 @@ public class Reg_PI extends JPanel {
 		lblFirstn.setBounds(10, 36, 70, 14);
 		add(lblFirstn);
 		
-		ssnTextField = new JTextField();
-		ssnTextField.setBounds(137, 108, 126, 20);
-		add(ssnTextField);
-		ssnTextField.setColumns(10);
+		firstNameTextField = new JTextField();
+		firstNameTextField.setColumns(10);
+		firstNameTextField.setBounds(137, 33, 126, 20);
+		add(firstNameTextField);
 						
 		JLabel lblLastName = new JLabel("Last Name");
 		lblLastName.setBounds(10, 61, 70, 14);
 		add(lblLastName);
 		
-		birthDateTextField = new JTextField();
-		birthDateTextField.setColumns(10);
-		birthDateTextField.setBounds(137, 83, 126, 20);
-		add(birthDateTextField);
+		lastNameTextField = new JTextField();
+		lastNameTextField.setColumns(10);
+		lastNameTextField.setBounds(137, 58, 126, 20);
+		add(lastNameTextField);
 
 		
 		JLabel lblBirthDate = new JLabel("Birth Date");
 		lblBirthDate.setBounds(10, 86, 70, 14);
 		add(lblBirthDate);
 		
-		lastNameTextField = new JTextField();
-		lastNameTextField.setColumns(10);
-		lastNameTextField.setBounds(137, 58, 126, 20);
-		add(lastNameTextField);
+		birthDateTextField = new JTextField();
+		birthDateTextField.setColumns(10);
+		birthDateTextField.setBounds(137, 83, 126, 20);
+		add(birthDateTextField);
+		
+		
 				
 		JLabel lblSocialSecurity = new JLabel("Soc Sec");
 		lblSocialSecurity.setBounds(10, 111, 70, 14);
 		add(lblSocialSecurity);
 		
-		firstNameTextField = new JTextField();
-		firstNameTextField.setColumns(10);
-		firstNameTextField.setBounds(137, 33, 126, 20);
-		add(firstNameTextField);
+		ssnTextField = new JTextField();
+		ssnTextField.setBounds(137, 108, 126, 20);
+		add(ssnTextField);
+		ssnTextField.setColumns(10);
+		
+		
 		
 		JLabel lblSex = new JLabel("Sex:");
 		lblSex.setBounds(10, 139, 70, 14);
@@ -73,15 +79,17 @@ public class Reg_PI extends JPanel {
 		save.setBounds(125, 176, 89, 23);
 		save.addActionListener(b);
 		add(save);
+		sex = new ButtonGroup();
+		maleCheck = new JRadioButton("Male");
+		maleCheck.setBounds(100, 135, 70, 23);
+		add(maleCheck);
 		
-		JRadioButton check1 = new JRadioButton("Male");
-		check1.setBounds(100, 135, 70, 23);
-		add(check1);
 		
-		JRadioButton check2 = new JRadioButton("Female");
-		check2.setBounds(188, 135, 70, 23);
-		add(check2);
-		
+		femaleCheck = new JRadioButton("Female");
+		femaleCheck.setBounds(188, 135, 70, 23);
+		add(femaleCheck);
+		sex.add(maleCheck);
+		sex.add(femaleCheck);
 		
 	}
 }
